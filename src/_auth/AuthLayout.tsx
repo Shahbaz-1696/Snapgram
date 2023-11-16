ea79e0a59d9d3be38ec27c1b4064d4077ed939e1
Shahbaz Ansari <ansarishahbaz96@gmail.com>
2023-11-17 00:08:03 +0530

    Added SignupForm and initial authentication

--- src/_auth/AuthLayout.tsx
+++ src/_auth/AuthLayout.tsx
@@ -7,9 +7,17 @@ const AuthLayout = () => {
       {isAuthenticated ? (
         <Navigate to="/" />
       ) : (
-        <section className="flex flex-1 justify-center items-center flex-col py-10">
-          <Outlet />
-        </section>
+        <>
+          <section className="flex flex-1 justify-center items-center flex-col py-10">
+            <Outlet />
+          </section>
+
+          <img
+            src="/assets/images/side-img.svg"
+            alt="logo"
+            className="hidden xl:block h-screen w-1/2 bg-no-repeat object-cover"
+          />
+        </>
       )}
     </>
   );
