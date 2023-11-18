0e806e4a47473e56540d2aa72d3d90bcf33eb1da
Shahbaz Ansari <ansarishahbaz96@gmail.com>
2023-11-18 23:32:29 +0530

    Added Sign In Logic using  appwrite

--- src/types/index.ts
+++ src/types/index.ts
@@ -4,3 +4,22 @@ export type INewUser = {
   email: string;
   password: string;
 };
+
+export type IUser = {
+  id: string;
+  name: string;
+  username: string;
+  email: string;
+  password: string;
+  bio: string;
+  imageUrl: string;
+};
+
+export type IContextType = {
+  user: IUser;
+  isAuthenticated: boolean;
+  isLoading: boolean;
+  setUser: React.Dispatch<React.SetStateAction<IUser>>;
+  setIsAuthenticated: React.Dispatch<React.SetStateAction<boolean>>;
+  checkAuthUser: () => Promise<boolean>;
+};
