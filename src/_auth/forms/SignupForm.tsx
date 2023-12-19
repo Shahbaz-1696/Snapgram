6241cadf5a3212b51e8e6b8b7b9292b305f0d1f5
Shahbaz Ansari <ansarishahbaz96@gmail.com>
2023-12-19 12:24:22 +0530

    bug fixes

--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -24,13 +24,12 @@ import { useUserContext } from "@/context/AuthContext";
 const SignupForm = () => {
   const { toast } = useToast();
   const navigate = useNavigate();
-  const { isLoading: isUserLoading, checkAuthUser } = useUserContext();
+  const { checkAuthUser } = useUserContext();
 
   const { mutateAsync: createUserAccount, isPending: isCreatingAccount } =
     useCreateUserAccount();
 
-  const { mutateAsync: signInAccount, isPending: isSigningIn } =
-    useSignInAccount();
+  const { mutateAsync: signInAccount } = useSignInAccount();
 
   // 1. Define your form.
   const form = useForm<z.infer<typeof SignupValidation>>({
--- src/_root/pages/AllUsers.tsx
+++ src/_root/pages/AllUsers.tsx
@@ -2,11 +2,7 @@ import Loader from "@/components/shared/Loader";
 import UserCard from "@/components/shared/UserCard";
 import { useGetPeopleUsers } from "@/lib/react-query/queries";
 const AllUsers = () => {
-  const {
-    data: creators,
-    isPending: isUserLoading,
-    isError: isErrorCreators,
-  } = useGetPeopleUsers(10);
+  const { data: creators, isPending: isUserLoading } = useGetPeopleUsers(10);
   return (
     <div className="flex flex-1">
       <div className="common-container">
