f2567c357334f2f0025e9c0e675bd569f689430e
Shahbaz Ansari <ansarishahbaz96@gmail.com>
2023-12-21 12:02:07 +0530

    Bug fixes

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
-import AuthProvider from "./context/AuthContext.tsx";
-import { QueryProvider } from "./lib/react-query/queryProvider.tsx";
+
+import { AuthProvider } from "@/context/AuthContext";
+import { QueryProvider } from "@/lib/react-query/QueryProvider";
+
+import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
